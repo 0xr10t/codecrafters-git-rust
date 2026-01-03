fb765a226735c6e59ffe2fbe492a72a7fcd7fefa
0xr10t <sohamvijaya1000@gmail.com>
2026-01-04 00:02:19 +0530

    applied some corrections

--- src/ls_tree.rs
+++ src/ls_tree.rs
@@ -13,18 +13,18 @@ pub fn name_only(hash:&String){
     let path = format!(".git/objects/{}/{}",dir_name,file_name);
     let mut object = fs::File::open(path).unwrap();
     let mut content: Vec<u8> = vec![];
-    let mut extracted_content = String::new();
+    let mut extracted_content: Vec<u8> = Vec::new();
     object.read_to_end(&mut content).unwrap();
     let mut decoder = ZlibDecoder::new(content.as_slice());
-    decoder.read_to_string(&mut extracted_content).unwrap();
-    let null_pos_w = extracted_content.find('\0');
-    let (_header, rest) = extracted_content.split_at(null_pos_w.unwrap());
-    let iter = rest.split_ascii_whitespace();
-    for (i,s) in iter.enumerate(){
-        if i%2==0{
-            let pos_w = s.find('\0');
-            let (name,_hash) = s.split_at(pos_w.unwrap());
-            println!("{name}");
-        }
+    decoder.read_to_end(&mut extracted_content).unwrap();
+    let header_end = extracted_content.iter().position(|&b| b==b'\0').unwrap();
+    let mut cursor = header_end+1;
+    while cursor<extracted_content.len(){
+        let mode_end = &extracted_content[cursor..].iter().position(|&b| b==b' ').unwrap() + cursor;
+        cursor = mode_end+1;
+        let name_end = &extracted_content[cursor..].iter().position(|&b| b== b'\0').unwrap()+cursor;
+        let name = str::from_utf8(&extracted_content[cursor..name_end]).unwrap();
+        println!("{name}");
+        cursor += 20;
     }
 }
