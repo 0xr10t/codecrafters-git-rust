0291796e129e74b8c7fa72e18c204928f7a3d39f
0xr10t <sohamvijaya1000@gmail.com>
2026-01-05 18:13:54 +0530

    almost done just some casees left

--- src/write_tree.rs
+++ src/write_tree.rs
@@ -1,10 +1,10 @@
-use std::fs::{self, read_dir};
+use std::fs;
 use std::path::Path;
 use std::fs::File;
 use std::io::Read;
 use sha1 ::{Sha1, Digest};
 use std::io::Write;
-use flate2::Compression;
+use flate2::{Compression};
 use flate2::write::ZlibEncoder;
 
 pub struct TreeEntry{
@@ -22,28 +22,21 @@ pub enum Mode{
 
 pub fn write_tree(){
     let root_sha = visit_dirs(Path::new("."));
-    let path = "./";
-    let mut modes:Vec<String> = Vec::new();
-    let mut names:Vec<String> = Vec::new();
-    let mut hashes:Vec<String> = Vec::new();
-    let entries = read_dir(path);
-    for entry in entries.unwrap() {
-            let entry = entry.unwrap();
-            let name = entry.file_name();
-            let mode = entry.file_type().unwrap();
-            let path = entry.path();
-            if path.to_str().unwrap().to_string() == ".git"{
-                continue;
-            }
-            if path.is_dir() {
-                visit_dirs(&path);
-            }
-            else{
-                hashes.push(write_self(&path.to_str().unwrap().to_string()));
-            }
-            // modes.push();
-        }
-        println!("{}", root_sha);
+    // let path = "./";
+    // let entries = read_dir(path);
+    // for entry in entries.unwrap() {
+    //         let entry = entry.unwrap();
+    //         // let name = entry.file_name();
+    //         // let mode = entry.file_type().unwrap();
+    //         let path = entry.path();
+    //         if path.to_str().unwrap().to_string() == ".git"{
+    //             continue;
+    //         }
+    //         if path.is_dir() {
+    //             visit_dirs(&path);
+    //         }
+    //     }
+    println!("{}", root_sha);
 }
 
 fn visit_dirs(dir: &Path) -> String{
@@ -76,8 +69,37 @@ fn visit_dirs(dir: &Path) -> String{
             }
         }
     }
-    let hash = String::new();
-    hash
+    entries.sort_by_key(|e| e.name.clone());
+    let mut body = Vec::new();
+
+    for entry in entries{
+        write!(body,
+            "{} {}\0",
+            entry.mode as u32,
+            entry.name       
+        ).unwrap();
+
+        let sha1_hash = hex::decode(entry.sha).unwrap();
+        body.extend_from_slice(&sha1_hash);
+    }
+
+    let mut tree = Vec::new();
+    write!(tree, "tree {}\0", body.len()).unwrap();
+    tree.extend_from_slice(&body);
+    // write_object(tree);
+    let mut hasher = Sha1::new();
+    hasher.update(&tree);
+    let hash_bytes = hasher.finalize();
+    let hash_hex = hex::encode(hash_bytes);
+    let (folder_name,file_name) = hash_hex.split_at(2);
+    let object_directory = format!(".git/objects/{}",folder_name);
+    let object_name = format!("{}/{}",object_directory,file_name);
+    fs::create_dir_all(&object_directory).unwrap();
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write_all(&tree).unwrap();
+    let compressed_bytes = e.finish().unwrap();
+    fs::write(object_name, compressed_bytes).unwrap();
+    hash_hex
 }
 
 pub fn write_self(filename: &String) -> String {
