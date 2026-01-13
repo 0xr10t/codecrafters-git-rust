af5fcd1d4cabfb7f7d46960c70749f22e0c2657f
0xr10t <sohamvijaya1000@gmail.com>
2026-01-14 02:19:51 +0530

    read pkt lines

--- src/clone.rs
+++ src/clone.rs
@@ -5,7 +5,7 @@
 //      ├─ Packfile decoder
 //      └─ Object writer (you already have this)
 
-use std::{collections::HashMap, env};
+use std::env;
 
 use crate::init::init;
 
@@ -23,13 +23,42 @@ pub async fn clone(args: &[String]){
             url.trim_end_matches('/')
         );
     let resp = get_req(&get_url).await;
+    let data = read_pkt_lines(&resp);
 }
 
-async fn get_req(url: &String) -> HashMap<String,String>{
+async fn get_req(url: &String) -> Vec<u8>{
     //http get requeste
     let resp = reqwest::get(url)
-        .await.unwrap()
-        .json::<HashMap<String, String>>()
         .await.unwrap();
-    resp
+        // .json::<HashMap<String, String>>()
+        // .await.unwrap();
+    resp.bytes().await.unwrap().to_vec()
+}
+
+// response format
+// 003f# service=git-upload-pack\n
+// 0000
+// 003e<sha> refs/heads/main\0<capabilities>\n
+// 003d<sha> refs/HEAD\n
+// 0000
+
+//convert packet-lines into readable data
+fn read_pkt_lines(resp: &[u8]) -> Vec<Vec<u8>>{
+    let mut lines = Vec::new();
+    let mut cursor = 0;
+
+    while cursor+4 < resp.len(){
+        let len = &resp[cursor..cursor+4];
+        let len_str = str::from_utf8(len).unwrap();
+        let len_usize = usize::from_str_radix(len_str, 16).unwrap();
+
+        cursor+=4;
+        if len_usize == 0{
+            break;
+        }
+        let payload_len = len_usize-4;
+        let payload = resp[cursor..cursor+payload_len].to_vec();
+        lines.push(payload);
+    }
+    lines
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use codecrafters_git::{cat_file::cat_file, clone::clone, commit_tree::commit_tree, hash_object::hash_object, init::init, ls_tree::ls_tree, write_tree::write_tree};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
 
@@ -13,7 +14,7 @@ fn main() {
         "ls-tree" => ls_tree(&args[2..]),
         "write-tree" => write_tree(),
         "commit-tree" =>  commit_tree(&args[2..]),
-        "clone" => clone(&args[2..]),
+        "clone" => clone(&args[2..]).await,
         _ => println!("unknown command: {}", args[1]),
     }
 }
